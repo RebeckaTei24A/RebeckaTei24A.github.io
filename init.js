837ff7aa959160216be985a45436fceff5c38358
RebeckaTei24A <rebkar080219@student.jenseneducation.se>
2025-12-03 12:50:37 +0100

    init

---
+++ init.js
@@ -0,0 +1,14 @@
+// Headern och footern laddas in i varje sida från denna fil
+
+let header = document.getElementsByTagName("header");
+
+ header[0].innerHTML =
+
+
+let footer =document.getElementsByTagName("footer")
+
+footer[0].innerHTML = `<p>&copy; 2025 Nails of Love by Rebecka - 
+        <a href="https://mail.google.com/mail/?view=cm&to=Nailsoflovebyrebecka@gmail.com" target="_blank" class="mail">Nailsoflovebyrebecka@gmail.com</a></p>`
+
+
+    
