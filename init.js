60906118cb927b57c7019bf2da07d7285706ccb8
RebeckaTei24A <rebkar080219@student.jenseneducation.se>
2025-12-17 13:53:11 +0100

    fixat bildernas storlek till344x500 och samtidigt ändrat de till webp istället för jgp (dator hemma)

--- init.js
+++ init.js
@@ -6,19 +6,28 @@
 
 // if (header) kollar om header finns innan vi fyller den med menyn, för att undvika fel.
 
+// && är och
 
 
-                    // HEADER
 
 document.addEventListener("DOMContentLoaded", () => {
+
+// ======================== HEADER ======================================
   const header = document.querySelector("header");
   if (header) {
   header.innerHTML = `
        <nav class="navbar" aria-label="Huvudmeny">
-            <a class="logo" href="index.html"><img src="Nails of Love.jpg" alt="Nails Of Love By Rebecka"></a>
-            <button class="burger" aria-label="Öppna meny" aria-expanded="false" aria-controls="primary-nav">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+            <a class="logo" href="index.html">
+             <img src="Nails of Love.webp" alt="Nails Of Love By Rebecka">
+            </a>
+
+            <button class="burger" 
+             aria-label="Öppna meny" 
+             aria-expanded="false" 
+             aria-controls="primary-nav">
+             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
             </button>
+
           <ul id="primary-nav" class="nav-links">
             <li><a href="index.html">Hem</a></li>
             <li><a href="about.html">Info</a></li>
@@ -33,25 +42,26 @@ document.addEventListener("DOMContentLoaded", () => {
             
       }
     
-                     // FOOTER
-
+// ======================= FOOTER ==============================================
   const footer = document.querySelector("footer");
   if (footer){
   footer.innerHTML = `
-  <p>&copy; 2025 Nails Of Love By Rebecka -
-  <a href="mailto:Nailsoflovebyrebecka@gmail.com" class="mail">Nailsoflovebyrebecka@gmail.com</a>
+  <p>
+  &copy; 2025 Nails Of Love By Rebecka -
+  <a href="mailto:Nailsoflovebyrebecka@gmail.com" class="mail">Nailsoflovebyrebecka@gmail.com
+  </a>
   </p>
   `;
   }
 
 
-
+// ======================= Burger Meny ==============================================
   const burger = document.querySelector(".burger");
   const navLinks = document.querySelector(".nav-links");
 
-  // && är och
-
   if (burger && navLinks) {
+
+    // Öppna/Stänga meny när man klickar på knappen "burger"
     burger.addEventListener("click", () => {
       const isOpen = navLinks.classList.toggle("show");
       burger.setAttribute("aria-expanded", isOpen ? "true" : "false");
@@ -61,7 +71,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // menyn stängs när man klickar på en länk
   navLinks.addEventListener("click", (e) => {
-    if (e.target.tagName === "A") {
+    if (e.target.closest("a")) {
       navLinks.classList.remove("show");
       burger.setAttribute("aria-expanded", "false");
       burger.setAttribute("aria-label", "Öppna meny");
@@ -69,5 +79,21 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 }
 
-});
 
+
+
+// =============================== Lazy Loading för bilder och video hos inspo ===========================================
+
+   const inspoGrid = document.querySelector(".grid-container");
+   if (inspoGrid) {
+    inspoGrid.querySelectorAll("img").forEach(img => {
+      img.setAttribute("loading", "lazy");
+    });
+
+    inspoGrid.querySelectorAll("video").forEach(video => {
+      video.setAttribute("preload", "metadata");
+    });
+   }
+
+
+   });
